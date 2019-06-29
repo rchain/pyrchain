916d8260d8862a887eabfe4908a6d384ff17ded3
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-06-29 22:14:30 +0200

    Update test.py to rchain.* API

--- test.py
+++ test.py
@@ -1,17 +1,31 @@
 import logging
+import hashlib
 from pprint import pprint
+
+from ecdsa import SigningKey
+from ecdsa.curves import SECP256k1
+from ecdsa.util import sigencode_der_canonize
 
 import grpc
 
-import DeployService_pb2
-import DeployService_pb2_grpc
+from rchain.pb import DeployService_pb2_grpc
+
+from rchain.crypto import PrivateKey, PublicKey
+from rchain.rpc import create_deploy_data
+
+
 
 def run():
-    with grpc.insecure_channel('node4.devnet.rchain-dev.tk:40401') as channel:
+    with grpc.insecure_channel('172.27.0.2:40401') as channel:
         stub = DeployService_pb2_grpc.DeployServiceStub(channel)
-        response = stub.getBlocks(DeployService_pb2.BlocksQuery(depth=1))
-        for x in response:
-            pprint(type(x.success.response.value))
+
+        term = '@"foo"!("Hi")'
+        key = PrivateKey.generate()
+        deploy_data = create_deploy_data(key, term, 1)
+
+        response = stub.DoDeploy(deploy_data)
+        print(response)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
