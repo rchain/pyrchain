7d23e322d749e8b6fe8e9d3d43a68884d52bca56
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-06-30 20:33:55 +0200

    Extend crypto (addresses, tests)

--- rchain/crypto.py
+++ rchain/crypto.py
@@ -1,8 +1,9 @@
 import hashlib
 
+import bitcoin.base58
 from ecdsa import SigningKey, VerifyingKey
 from ecdsa.curves import SECP256k1
-from ecdsa.util import sigencode_der_canonize
+from ecdsa.util import sigencode_der_canonize, sigdecode_der
 
 
 def blake2b_32(data=b''):
@@ -10,23 +11,57 @@ def blake2b_32(data=b''):
 
 
 class PublicKey:
+
+    @classmethod
+    def from_bytes(cls, b: bytes) -> 'PublicKey':
+        if len(b) == 65:
+            b = b[1:]  # remove 0x04 prefix
+        return cls(VerifyingKey.from_string(b, curve=SECP256k1))
+
+    @classmethod
+    def from_hex(cls, s: str) -> 'PublicKey':
+        return cls.from_bytes(bytes.fromhex(s))
+
     def __init__(self, _pub_key: VerifyingKey):
         self._pub_key = _pub_key
+
+    def verify(self, signature: bytes, data: bytes):
+        self._pub_key.verify(signature, data, hashfunc=blake2b_32, sigdecode=sigdecode_der)
 
     def to_bytes(self) -> bytes:
         return b'\x04' + self._pub_key.to_string()
 
+    def get_address(self) -> str:
+        prefix = b'\0\0\0\0'
+        pub_key_hash = blake2b_32(self.to_bytes()).digest()
+        payload = prefix + pub_key_hash
+        payload_chksum = blake2b_32(payload).digest()[:4]
+        addr_bytes = payload + payload_chksum
+        return bitcoin.base58.encode(addr_bytes)
+
 
 class PrivateKey:
+
     @classmethod
     def generate(cls) -> 'PrivateKey':
         return cls(SigningKey.generate(curve=SECP256k1))
+
+    @classmethod
+    def from_bytes(cls, b: bytes) -> 'PrivateKey':
+        return cls(SigningKey.from_string(b, curve=SECP256k1))
+
+    @classmethod
+    def from_hex(cls, s: str) -> 'PrivateKey':
+        return cls.from_bytes(bytes.fromhex(s))
 
     def __init__(self, _key: SigningKey):
         self._key = _key
 
     def sign(self, data: bytes) -> bytes:
         return self._key.sign(data, hashfunc=blake2b_32, sigencode=sigencode_der_canonize)
+
+    def to_bytes(self) -> bytes:
+        return self._key.to_string()
 
     def get_public_key(self) -> PublicKey:
         return PublicKey(self._key.get_verifying_key())
--- rchain/crypto_test.py
+++ rchain/crypto_test.py
@@ -1,9 +1,12 @@
 import unittest
 
-from .crypto import blake2b_32
+from ecdsa.keys import BadSignatureError
+
+from .crypto import blake2b_32, PrivateKey, PublicKey
 
 
 class TestCrypto(unittest.TestCase):
+
     def test_blake2b_32(self):
         in_out_list = [
             ('', '0e5751c026e543b2e8ab2eb06099daa1d1e5df47778f7787faab45cdf12fe3a8'),
@@ -16,3 +19,60 @@ class TestCrypto(unittest.TestCase):
         for input_str, expected_output in in_out_list:
             actual_output = blake2b_32(input_str.encode()).digest().hex()
             self.assertEqual(expected_output, actual_output)
+
+    identities = [
+        (
+            '1000000000000000000000000000000000000000000000000000000000000000',
+            '0408ea9666139527a8c1dd94ce4f071fd23c8b350c5a4bb33748c4ba111faccae0620efabbc8ee2782e24e7c0cfb95c5d735b783be9cf0f8e955af34a30e62b945',
+            '1111sc15PERLugDRdfPHkSfYPpar8i1NcwM5hKsE64HVUG2zCF5sy'
+        ),
+        (
+            '1111111111111111111111111111111111111111111111111111111111111111',
+            '044f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa385b6b1b8ead809ca67454d9683fcf2ba03456d6fe2c4abe2b07f0fbdbb2f1c1',
+            '1111GcGEJX6me9vVN5cPGbFHSXepo8BvQ9r9CFcNmYSemHDvdpPZB'
+        ),
+        (
+            '2222222222222222222222222222222222222222222222222222222222222222',
+            '04466d7fcae563e5cb09a0d1870bb580344804617879a14949cf22285f1bae3f276728176c3c6431f8eeda4538dc37c865e2784f3a9e77d044f33e407797e1278a',
+            '11112JaqFvFpwWBEUc1jhLRkY2BuF7vgMxYCc3hepqdj2ad9U7Lg3g'
+        )
+    ]
+
+    def test_privatekey_from_hex(self):
+        for key_hex, expected_pub_key_hex, _ in self.identities:
+            key = PrivateKey.from_hex(key_hex)
+            pub_key = key.get_public_key()
+            actual_pub_key_hex = pub_key.to_bytes().hex()
+            self.assertEqual(expected_pub_key_hex, actual_pub_key_hex)
+
+    def test_publickey_from_hex(self):
+        for key_hex, pub_key_hex, _ in self.identities:
+            key = PrivateKey.from_hex(key_hex)
+            pub_key = PublicKey.from_hex(pub_key_hex)
+            expected_pub_key_bytes = key.get_public_key().to_bytes()
+            actual_pub_key_bytes = pub_key.to_bytes()
+            self.assertEqual(expected_pub_key_bytes, actual_pub_key_bytes)
+
+    def test_sign_verify_good(self):
+        for key_hex, pub_key_hex, _ in self.identities:
+            key = PrivateKey.from_hex(key_hex)
+            pub_key = PublicKey.from_hex(pub_key_hex)
+            message = 'hello rchain'.encode()
+            signature = key.sign(message)
+            pub_key.verify(signature, message)
+
+    def test_sign_verify_bad(self):
+        for key_hex, pub_key_hex, _ in self.identities:
+            key = PrivateKey.from_hex(key_hex)
+            pub_key = PublicKey.from_hex(pub_key_hex)
+            sign_message = 'hello rchain'.encode()
+            verify_message = 'hello world'.encode()
+            signature = key.sign(sign_message)
+            with self.assertRaises(BadSignatureError):
+                pub_key.verify(signature, verify_message)
+
+    def test_address(self):
+        for _, pub_key_hex, expected_address in self.identities:
+            pub_key = PublicKey.from_hex(pub_key_hex)
+            actual_address = pub_key.get_address()
+            self.assertEqual(expected_address, actual_address)
