abffbfa328200e818c73122df00165ec459c10ed
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-07-01 15:12:36 +0200

    Add hardnet util module

---
+++ rchain/hardnet/util.py
@@ -0,0 +1,5 @@
+NATO_PHONETIC_ALPHABET = '''
+    Alfa Bravo Charlie Delta Echo Foxtrot Golf Hotel India Juliett Kilo Lima
+    Mike November Oscar Papa Quebec Romeo Sierra Tango Uniform Victor Whiskey
+    Xray Yankee Zulu
+'''.strip().split()
