3afe2056a4be337f33316f727e7b66cd1d1b328a
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-07-11 00:40:26 +0200

    Bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name='pyrchain',
-    version='0.1.3',
+    version='0.1.4',
     author='RChain Cooperative',
     author_email='rchain-makers@rchain.coop',
     description='Interface to RChain RNode RPC',
