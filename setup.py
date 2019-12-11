e2346cfa8f400c0199279e757539a88e3b8f508e
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-12-11 10:30:46 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='pyrchain',
-    version='0.1.6',
+    version='0.2.0',
     author='RChain Cooperative',
     author_email='rchain-makers@rchain.coop',
     description='Interface to RChain RNode RPC',
