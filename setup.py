728f5787a1a32381c0219cbdebeb033a2809b127
Will Qiu <zsluedem06@gmail.com>
2020-03-08 17:55:06 +0800

    python3.6 install dataclasses

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setuptools.setup(
         'cryptography',
         'eth_hash',
         'pycryptodome',
-        'eth-keyfile'
+        'eth-keyfile',
+        'dataclasses'
     ],
     extras_require={
         'dev': ['grpcio-tools', 'mypy', 'typing-extensions', 'mypy-protobuf', 'isort', 'pytest', 'sphinx']},
