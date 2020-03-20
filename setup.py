d1126d52da77c4e0241e60c4439d009c1eb70133
Will Qiu <zsluedem06@gmail.com>
2020-03-20 11:06:34 +0800

    bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='pyrchain',
-    version='0.3.0',
+    version='0.3.1',
     author='RChain Cooperative',
     author_email='rchain-makers@rchain.coop',
     description='Interface to RChain RNode RPC',
