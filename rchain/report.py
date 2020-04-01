e0f28e8247ab9043287135525fc431fb781eab3b
Will Qiu <zsluedem06@gmail.com>
2020-04-01 10:57:51 +0800

    add get transaction api and get blocks by heights

---
+++ rchain/report.py
@@ -0,0 +1,18 @@
+from .pb.DeployServiceCommon_pb2 import SingleReport
+from .pb.RhoTypes_pb2 import Par, ParWithRandom
+from dataclasses import dataclass
+from typing import Optional, Tuple
+
+@dataclass
+class Report:
+    precharge: SingleReport
+    user: SingleReport
+    refund: SingleReport
+
+@dataclass
+class Transaction:
+    from_addr: str
+    to_addr: str
+    amount: int
+    ret_unforgeable: Par
+    success: Optional[Tuple[bool, str]]
