c43e96129199e7d051824750e5e84bf65922d559
Will Qiu <zsluedem06@gmail.com>
2021-01-07 19:33:54 +0800

    create insert signed example

---
+++ examples/client_insert_signed.py
@@ -0,0 +1,56 @@
+# https://github.com/tgrospic/rnode-client-js/blob/master/src/nodejs/client-insert-signed.js
+# the logic below mostly copied from the logic of the link above
+
+from rchain.crypto import PrivateKey, blake2b_32
+from rchain.client import RClient
+from rchain.pb.RhoTypes_pb2 import Par, Expr, ETuple, Bundle, GPrivate, GUnforgeable
+
+host = "localhost"
+port = 40402
+
+deployKey = PrivateKey.generate()
+timestamp = 1559156356769
+nonce = 9223372036854775807
+publicKey = deployKey.get_public_key()
+
+with RClient(host, port) as client:
+    ret = client.previewPrivateNames(publicKey, timestamp, 3)
+    unforgeable = ret.payload.ids[0]
+
+print(unforgeable)
+dataToSign = Par(
+    exprs=[
+        Expr(e_tuple_body=
+        ETuple(ps=[
+            Par(exprs=[Expr(g_int=nonce)]),
+            Par(bundles=[
+                Bundle(body=Par(unforgeables=[GUnforgeable(g_private_body=GPrivate(id=unforgeable))]),
+                       writeFlag=True,
+                       readFlag=False)])
+        ])
+        )
+    ]
+)
+sigArray = deployKey.sign_deterministic(dataToSign.SerializeToString())
+signatureHex = sigArray.hex()
+publicKeyHex = publicKey.to_hex()
+
+contract = """
+    new MyContract, rs(`rho:registry:insertSigned:secp256k1`), uriOut, out(`rho:io:stdout`)
+    in {{
+      contract MyContract(ret) = {{
+        ret!("Hello Arthur!")
+      }} |
+      rs!(
+        "{publicKeyHex}".hexToBytes(),
+        ({nonce}, bundle+{{*MyContract}}),
+        "{signatureHex}".hexToBytes(),
+        *uriOut
+      ) |
+      for(@uri <- uriOut) {{
+        out!(("Registered", uri))
+      }}
+    }}
+""".format(publicKeyHex=publicKeyHex, nonce=nonce, signatureHex=signatureHex)
+
+print(contract)
